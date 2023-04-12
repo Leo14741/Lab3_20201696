04543c6d2fed318ea0bcfe33977b3fa004de96b7
Leonardo <a20201696@pucp.edu.pe>
2023-04-11 20:46:23 -0500

    Initial commit

---
+++ src/test/java/com/example/lab_20201696/Lab20201696ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab_20201696;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab20201696ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
